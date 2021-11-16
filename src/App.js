f697eef02f3505eaa817f6357a7ae21b4ccefc42
1hakann <hakan.cakmak@live.com>
2021-11-17 00:18:49 +0300

    give onclick to button and write css for button

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <section className="container">
         <h3>{people.length} Birthday Today</h3>
         <List people={ people } />
-       
+       <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
